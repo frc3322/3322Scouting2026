2f9d5dffe21cf67fc4569b0c0cabcc035be2fb09
Sean Meng <seanmeng2018@gmail.com>
2026-01-24 13:14:08 -0500

    Added datahandler

---
+++ match scouting/DataHandler.js
@@ -0,0 +1,30 @@
+
+
+class DataHandler{
+    #autoFuel
+    #teleFuel
+    
+    
+    constructor(){
+        this.#autoFuel = 0;
+        this.#teleFuel = 0;
+    }
+
+    incrementAutoFuel(val){
+        this.#autoFuel += val;
+    }
+
+    incrementTeleFuel(val){
+        this.#teleFuel += val;
+    }
+
+    getAutoFuel(){
+        return this.#autoFuel;
+    }
+
+    getTeleFuel(val){
+        return this.#teleFuel;
+    }
+
+
+}
