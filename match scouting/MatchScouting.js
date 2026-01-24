1024180280a52846947f0932de15639713f02f13
Sean Meng <seanmeng2018@gmail.com>
2026-01-24 11:02:31 -0500

    localstorage test

---
+++ match scouting/MatchScouting.js
@@ -0,0 +1,13 @@
+
+
+function setButton(val){
+    localStorage.setItem("number", val);
+}
+
+function getButton(){
+    console.log(localStorage.getItem("number"));
+}
+
+function getTeleopButton(){
+     console.log(localStorage.getItem("number"));
+}
