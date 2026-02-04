a29557248f79e9c420a2e1f1e590c27d468b81e9
Waluigi6 <123126370+Waluigi6@users.noreply.github.com>
2026-02-04 17:42:45 -0500

    Update fuel and passing counters in teleop2.js

--- match-scouting/teleop2.js
+++ match-scouting/teleop2.js
@@ -23,24 +23,44 @@ document.getElementById("passingCounter").textContent = dataHandler.getTelePass(
 
 
 function increaseNumber(amount) {
+    
     dataHandler.incrementTele(ActionType.TeleFuel, amount);
     console.log(dataHandler.getTeleFuel());
-    document.getElementById("fuelCount").textContent = dataHandler.getTeleFuel();
+    document.getElementById("fuelCounter").textContent = dataHandler.getTeleFuel();
+    localStorage.setItem("dataHandler", dataHandler.toString());
+}
+function increasePassing(amount) {
+    
+    dataHandler.incrementTele(ActionType.TelePass, amount);
+    console.log(dataHandler.getTelePass());
+    document.getElementById("passingCounter").textContent = dataHandler.getTelePass();
     localStorage.setItem("dataHandler", dataHandler.toString());
 }
 
 function setButtons() {
-
-    document.getElementById("fuelHubSuccessIncrease1").addEventListener("click", () => {
+    
+    document.getElementById("hubSuccessIncrease1").addEventListener("click", () => {
         increaseNumber(1);
     });
 
-    document.getElementById("fuelHubSuccessIncrease5").addEventListener("click", () => {
+    document.getElementById("hubSuccessIncrease5").addEventListener("click", () => {
         increaseNumber(5);
     });
 
-    document.getElementById("fuelHubSuccessIncrease10").addEventListener("click", () => {
+    document.getElementById("hubSuccessIncrease10").addEventListener("click", () => {
         increaseNumber(10);
+    });
+
+    document.getElementById("fuelHubSuccessIncrease1").addEventListener("click", () => {
+        increasePassing(1);
+    });
+
+    document.getElementById("fuelHubSuccessIncrease5").addEventListener("click", () => {
+        increasePassing(5);
+    });
+
+    document.getElementById("fuelHubSuccessIncrease10").addEventListener("click", () => {
+        increasePassing(10);
     });
 
 
