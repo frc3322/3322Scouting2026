17c5a0f05945543984c2c2c8bb63820217580b2b
Waluigi6 <123126370+Waluigi6@users.noreply.github.com>
2026-02-04 17:43:12 -0500

    Add cycle counter increment and logging

--- match-scouting/teleop3.js
+++ match-scouting/teleop3.js
@@ -4,11 +4,15 @@ import { DataHandler } from "./DataHandler.js";
 var dataHandler = new DataHandler();
 dataHandler.loadData(localStorage.getItem("dataHandler"))
 
+
+alert(dataHandler.getTeleCycleCounter());
+
 var fuelCounter = document.getElementById("fuelCounter");
 var fuelIncrements = document.getElementById("fuelIncrements");
 
 fuelCounter.textContent = dataHandler.getTeleFuel();
 document.getElementById("cyclesCounter").textContent = dataHandler.getTeleCycleCounter();
+
 
 console.log(localStorage.getItem("dataHandler"));
 
@@ -31,9 +35,16 @@ function submitData() {
 
 function setButtons() {
     document.getElementById("submitShotBalls").addEventListener("click", () => {
+        document.getElementById("cyclesCounter").textContent = parseInt(document.getElementById("cyclesCounter").textContent) + parseInt(1);
+        dataHandler.setTeleCycleCounter(parseInt(document.getElementById("cyclesCounter").textContent));
         dataHandler.incrementTele(ActionType.TeleFuel, parseInt(fuelIncrements.value));
         updateDataHandler()
         fuelCounter.textContent = dataHandler.getTeleFuel();
+        console.log("Cycle counter: " + dataHandler.getTeleCycleCounter());
+        document.getElementById("cyclesCounter").textContent = dataHandler.getTeleCycleCounter();
+        updateDataHandler()
+
+
         console.log(localStorage.getItem("dataHandler"));
     });
 
