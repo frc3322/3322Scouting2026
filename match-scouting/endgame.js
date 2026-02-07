9a2a2095d79ee9bbfeccb913214ab4f59de89bf9
Dillon BT <Dillon.Bhuva.Trombley@gmail.com>
2026-02-07 13:00:38 -0500

    Made some changes to the Endgame

---
+++ match-scouting/endgame.js
@@ -0,0 +1,66 @@
+let stars;
+
+
+function updateStars(stars) {
+    if(stars >= 1) {
+        document.getElementById("rate1").innerHTML = "&#9733";
+    } else {
+        document.getElementById("rate1").innerHTML = "&#9734";
+    }
+    if(stars >= 2) {
+        document.getElementById("rate2").innerHTML = "&#9733";
+    } else {
+        document.getElementById("rate2").innerHTML = "&#9734";
+    }
+    if(stars >= 3) {
+        document.getElementById("rate3").innerHTML = "&#9733";
+    } else {
+        document.getElementById("rate3").innerHTML = "&#9734";
+    }
+    if(stars >= 4) {
+        document.getElementById("rate4").innerHTML = "&#9733";
+    } else {
+        document.getElementById("rate4").innerHTML = "&#9734";
+    }
+    if(stars >= 5) {
+        document.getElementById("rate5").innerHTML = "&#9733";
+    } else {
+        document.getElementById("rate5").innerHTML = "&#9734";
+    }
+    if(stars == 0) {
+        document.getElementById("rate1").innerHTML = "&nbsp;";
+        document.getElementById("rate2").innerHTML = "&nbsp;";
+        document.getElementById("rate3").innerHTML = "&nbsp;";
+        document.getElementById("rate4").innerHTML = "&nbsp;";
+        document.getElementById("rate5").innerHTML = "&nbsp;";
+    }
+}
+
+function setButtons() {
+    
+    document.getElementById("rate1").addEventListener("click", () => {
+        updateStars(1);
+    });
+    document.getElementById("rate2").addEventListener("click", () => {
+        updateStars(2);
+    });
+    document.getElementById("rate3").addEventListener("click", () => {
+        updateStars(3);
+    });
+    document.getElementById("rate4").addEventListener("click", () => {
+        updateStars(4);
+    });
+    document.getElementById("rate5").addEventListener("click", () => {
+        updateStars(5);
+    });
+    document.getElementById("defence").addEventListener("click", () => {
+        if(document.getElementById("defence").checked) {
+            updateStars(5);
+        } else {
+        updateStars(0);
+        }
+    });
+
+}
+
+setButtons();
