7945f066f2e257c15063d4b612262b6be7355bd3
Sean Meng <seanmeng2018@gmail.com>
2026-02-14 15:45:54 -0500

    added animated shooting scouting

--- match-scouting/teleop3.js
+++ match-scouting/teleop3.js
@@ -2,52 +2,137 @@ import { ActionType } from "./Constants.js";
 import { DataHandler } from "./DataHandler.js";
 
 var dataHandler = new DataHandler();
-dataHandler.loadData(localStorage.getItem("dataHandler"))
+dataHandler.loadData(localStorage.getItem("dataHandler"));
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var teleFuel = 0;
+
+var slider = document.getElementById("slider");
+var sliderNumber = document.getElementById("sliderNumber");
+
+const canvasHeight = window.innerWidth * (620/1080) * 0.75;
+const canvasWidth = window.innerWidth * 0.75;
+
+ctx.canvas.height  = canvasHeight ;
+ctx.canvas.width = canvasWidth;
+
+var ball = new Image;
+ball.src = '../images/ball.svg'
+
+var bobot = new Image;
+bobot.src = '../images/bobot.svg'
+bobot.onload = () => {  
+        ctx.drawImage(bobot, 0, canvasHeight - 300*s, 300, 300);  
+      };
+
+
+var hub = new Image;
+hub.src = '../images/hub.svg'
+hub.onload = () => {  
+        ctx.drawImage(hub, canvasWidth-300, canvasHeight - 300, 300, 300);  
+      };
 
 
 
-var fuelCounter = document.getElementById("fuelCounter");
-var fuelIncrements = document.getElementById("fuelIncrements");
+const s = canvasWidth/1080;
 
-fuelCounter.textContent = dataHandler.getTeleFuel();
-document.getElementById("cyclesCounter").textContent = dataHandler.getTeleCycleCounter();
+console.log(canvasHeight, canvasWidth);
 
-
-console.log(localStorage.getItem("dataHandler"));
+var ballList = [[140*s,canvasHeight-120*s]]
 
 function updateDataHandler(){
     localStorage.setItem("dataHandler", dataHandler.toString());
 }
 
+ctx.font = "" + (40 * s) + "px sans-serif";
+function draw(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+  
+    
+    for(let i = ballList.length-1; i >= 0; i--){
+        
+        ctx.drawImage(ball, ballList[i][0], ballList[i][1], 50*s, 50*s);
+        ballList[i][0] += 13*s;
+        let t = (ballList[i][0]-140*s)/13
+        ballList[i][1] += t - 30*s;
+        if(ballList[i][1] > canvas.height-200*s && 
+            ballList[i][0] > canvas.width-400*s){
+            ballList.shift();
+            teleFuel += 1;
+        }
+    }
 
-function submitData() {
-    // Gather all data
+    ctx.drawImage(bobot, 0, canvasHeight - 300*s, 300*s, 300*s);
+    ctx.drawImage(hub, canvasWidth-425*s, canvasHeight - 400*s, 400*s, 400*s); 
+    
+    var textWidth = ctx.measureText("" + teleFuel ).width;
 
-    //datalist.push(document.getElementById("matchNumber").value + "");
+    ctx.fillText(teleFuel, canvasWidth-(215)*s - textWidth/2, canvasHeight - 100*s);
+    console.log("fwie")
 
-
-
-    // Generate QR code linking to the CSV data URL
-
-    window.location.href = "../qrcode.html" + "?data=" + document.getElementById("ballsCounter").textContent;
+    
+    window.requestAnimationFrame(draw);
 }
 
 function setButtons() {
-    document.getElementById("submitShotBalls").addEventListener("click", () => {
-        document.getElementById("cyclesCounter").textContent = parseInt(document.getElementById("cyclesCounter").textContent) + parseInt(1);
-        dataHandler.setTeleCycleCounter(parseInt(document.getElementById("cyclesCounter").textContent));
-        dataHandler.incrementTele(ActionType.TeleFuel, parseInt(fuelIncrements.value));
-        updateDataHandler()
-        fuelCounter.textContent = dataHandler.getTeleFuel();
-        console.log("Cycle counter: " + dataHandler.getTeleCycleCounter());
-        document.getElementById("cyclesCounter").textContent = dataHandler.getTeleCycleCounter();
-        updateDataHandler()
-
-
-        console.log(localStorage.getItem("dataHandler"));
-    });
 
 }
 
+function newBall(){
+    ballList.push([140*s,canvasHeight-120*s]);
+}
+
+var shootInterval = setInterval(newBall, 1000);
+clearInterval(shootInterval);
+
+var currentSliderValue = 0;
+
+function updateRate(){
+    if(slider.value != 0){ 
+        if(currentSliderValue != slider.value){
+            clearInterval(shootInterval);
+            shootInterval = setInterval(newBall, Math.sqrt(100/slider.value)*100);
+            currentSliderValue = slider.value;
+        }   
+    }
+    else{
+        clearInterval(shootInterval);
+    }
+    sliderNumber.textContent =  Math.sqrt(slider.value).toFixed(2) + " b/s";
+}
+
+function init(){
+    window.requestAnimationFrame(draw);
+    setInterval(updateRate, 250);
+}
+
+init();
+
+/*
+function dragDot(x,y){
+    if(mouseDown){
+        dot.style.left = (x-dot.offsetWidth/2) + "px";
+        dot.style.top = (y-dot.offsetHeight/2) + "px";
+        
+    }
+    
+}
+
+
+dot.addEventListener("mousedown", () => {
+    mouseDown = true;
+});
+
+dot.addEventListener("mouseup", () => {
+    mouseDown = false;
+});
+
+document.addEventListener("mousemove", (e)=>{
+    dragDot(e.clientX, e.clientY);
+});
+
+*/
 
 setButtons();
