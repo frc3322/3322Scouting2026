66cd45973e90200954fc961948782985b8a05ec2
Dillon BT <Dillon.Bhuva.Trombley@gmail.com>
2026-02-28 13:47:59 -0500

    The data recovery is working now

--- match-scouting/buttonField.js
+++ match-scouting/buttonField.js
@@ -1,3 +1,9 @@
+import { ActionType } from "./Constants.js";
+import { DataHandler } from "./DataHandler.js";
+
+var dataHandler = new DataHandler();
+dataHandler.loadData(localStorage.getItem("dataHandler"));
+
 const leftButton = document.querySelector("#leftMostButton");
 const rightButton = document.querySelector("#rightMostButton");
 const midLeftButtonOne = document.querySelector("#midLeft1");
@@ -23,8 +29,59 @@ midRightButtonThree,
 midRightButtonFour
 ];
 
+
+
+function setButtons() {
+    
+    document.getElementById("leftMostButton").addEventListener("click", () => {
+        console.log("Left button clicked");
+        dataHandler.setBooleanValue(8, true);
+    });
+    document.getElementById("rightMostButton").addEventListener("click", () => {
+        console.log("Right button clicked");
+        dataHandler.setBooleanValue(9, true);
+    });
+
+    document.getElementById("mdLeft1").addEventListener("click", () => {
+        console.log("Mid left button 1 clicked");
+        dataHandler.setBooleanValue(10, true);
+    }); 
+    document.getElementById("midLeft2").addEventListener("click", () => {
+        console.log("Mid left button 2 clicked");
+        dataHandler.setBooleanValue(11, true);
+    });
+    document.getElementById("midLeft3").addEventListener("click", () => {
+        console.log("Mid left button 3 clicked");
+        dataHandler.setBooleanValue(12, true);
+    });
+    document.getElementById("midLeft4").addEventListener("click", () => {
+        console.log("Mid left button 4 clicked");
+        dataHandler.setBooleanValue(13, true);
+    });
+
+    document.getElementById("midRight1").addEventListener("click", () => {
+        console.log("Mid right button 1 clicked");
+        dataHandler.setBooleanValue(14, true);
+    });
+    document.getElementById("midRight2").addEventListener("click", () => {
+        console.log("Mid right button 2 clicked");
+        dataHandler.setBooleanValue(15, true);
+    });
+    document.getElementById("midRight3").addEventListener("click", () => {
+        console.log("Mid right button 3 clicked");
+        dataHandler.setBooleanValue(16, true);
+    });
+    document.getElementById("midRight4").addEventListener("click", () => {
+        console.log("Mid right button 4 clicked");
+        dataHandler.setBooleanValue(17, true);
+    });
+}
+
+setButtons();
+
 allButtons.forEach(button => {
     button.addEventListener("click", () => {
-        window.open("", "_blank");         
+        console.log(dataHandler.toString());    
+        localStorage.setItem("dataHandler", dataHandler.toString());
     });
 });
