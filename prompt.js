61c332eea5011642938ab16ad23fe64f24989e56
Medhansh <medhansh.vrat@gmail.com>
2026-03-01 16:28:08 -0500

    updatedPromptSystem

---
+++ prompt.js
@@ -0,0 +1,10 @@
+import { DataHandler } from "./match-scouting/DataHandler.js";
+
+var dataHandler = new DataHandler();
+dataHandler.loadData(localStorage.getItem("dataHandler"));
+
+document.querySelector(".logo").addEventListener("click", () => {
+    
+});
+
+
