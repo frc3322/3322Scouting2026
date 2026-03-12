499ea27fc80e64faff3e52fa5918644794265714
Dillon BT <Dillon.Bhuva.Trombley@gmail.com>
2026-03-12 18:55:19 -0400

    I changed the defence so that it is better and it is in the defence.js tab

--- scripts/match-scouting/defense.js
+++ scripts/match-scouting/defense.js
@@ -1,6 +1,31 @@
 import { DataContainer } from "../data/DataContainer.js";
 import { DataHandler } from "../data/DataHandler.js";
 
+    let downwardsCounter = 0;
+
+    function changeCount(amount) {
+      const counter = document.getElementById('CyclesCounter');
+      let count = parseInt(counter.textContent);
+      count += amount;
+      if(count < 0)
+      {
+        count = 0;
+        downwardsCounter++;
+      }
+      counter.textContent = count;
+      if(downwardsCounter == 10)
+      {
+        alert("What are we doing gang? The counter only goes down to 0.");
+        downwardsCounter += 1;
+      }
+      if(downwardsCounter == 20)
+      {
+        alert("I'm genuinly gonna report you to Stone Cold Joe. Get better at scouting little bro.");
+        downwardsCounter += 1;
+      }
+      }
+    
+
 
 var dataContainer = new DataContainer();
 var dataHandler = new DataHandler(dataContainer);
@@ -13,5 +38,11 @@ catch{}
 
 
 function setButtons(){
-    
+document.getElementById("up1").addEventListener('click', () => changeCount(1));
+document.getElementById("down1").addEventListener('click', () => changeCount(-1));
+
 }
+
+
+
+setButtons();
